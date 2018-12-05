47b0633b46b1f449215d913735ea4bb6741813e7
Ramazan <39922910+kzramazan@users.noreply.github.com>
2018-12-05 21:17:26 +0600

    Add files via upload

---
+++ recognizer.py
@@ -0,0 +1,33 @@
+import cv2
+import numpy as np
+
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+
+cascadePath = "haarcascade_frontalface_default.xml"
+faceCascade = cv2.CascadeClassifier(cascadePath)
+recognizer.read('trainner/trainner.yml')
+
+cam = cv2.VideoCapture(0)
+font = cv2.FONT_HERSHEY_SIMPLEX
+while True:
+    ret, im =cam.read()
+    gray=cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
+    faces=faceCascade.detectMultiScale(gray, 1.2,5)
+    for(x,y,w,h) in faces:
+        cv2.rectangle(im,(x,y),(x+w,y+h),(225,0,0),2)
+        Id, conf = recognizer.predict(gray[y:y+h,x:x+w])
+        #Note to change this recognizer till submitting
+        if(conf<60):
+            if(Id==1):
+                Id="Aibol"
+            elif(Id==10):
+                Id="Ramazan"
+        else:
+            Id="Unknown"
+        cv2.putText(im,str(Id), (x,y+h),font, 255,  (255, 255, 255))
+        print(Id);
+    cv2.imshow('im',im) 
+    if cv2.waitKey(10) & 0xFF==ord('q'):
+        break
+cam.release()
+cv2.destroyAllWindows()
